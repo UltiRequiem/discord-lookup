7e004d14efe841a2a975a873a8c214a603835f9d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-05 15:44:59 -0500

    :sparkles: work work work

--- mod.ts
+++ mod.ts
@@ -1,3 +1,45 @@
-import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+import { serve, Handler } from "https://deno.land/std@0.145.0/http/server.ts";
 
-serve((req: Request) => new Response("Hello World"));
+// import "https://deno.land/std@0.151.0/dotenv/load.ts";
+
+const TOKEN = Deno.env.get("DISCORD_TOKEN");
+
+async function getUser(id: string | number) {
+  const response = await fetch(`https://discord.com/api/v9/users/${id}`, {
+    headers: {
+      Authorization: `Bot ${TOKEN}`,
+    },
+  });
+
+  const data = response.json();
+
+  return data;
+}
+
+const server: Handler = async (request) => {
+  if (request.method !== "POST") {
+    return new Response(undefined, { status: 405 });
+  }
+
+  const { pathname } = new URL(request.url);
+
+  if (pathname !== "/") {
+    return new Response(undefined, { status: 404 });
+  }
+
+  if (!request.body) {
+    return new Response(undefined, { status: 422 });
+  }
+
+  const body = await request.json();
+
+  if (!body.id) {
+    return new Response(undefined, { status: 422 });
+  }
+
+  const data = await getUser(body.id);
+
+  return new Response(JSON.stringify(data));
+};
+
+serve(server);
